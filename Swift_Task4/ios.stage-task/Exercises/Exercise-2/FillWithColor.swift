184d5bc4744edba736dddefeb60f18ce90b2922e
Dmitry Suhoversky <dmitrysuhov@outlook.com>
2021-06-06 09:50:26 +0300

    feat: two tasks complete

--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -3,6 +3,75 @@ import Foundation
 final class FillWithColor {
     
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        
+        let m = image.count
+        let n = image[row].count
+        var resulIimage = image
+        let oldColor = image[row][column]
+        var queue = [Element(row: row, column: column)]
+        var issueElements = Array<Element>()
+        
+        if (row < 0 || row >= m || column < 0 || column >= n)
+        {
+            return image
+        }
+        
+        while queue.count > 0
+        {
+            let element = queue.remove(at: 0)
+            issueElements.append(element)
+            
+            if (image[element.row][element.column] == oldColor)
+            {
+                resulIimage[element.row][element.column] = newColor
+            }
+            else
+            {
+                continue
+            }
+            
+            if (element.row - 1) >= 0
+            {
+                let element = Element(row: element.row - 1, column: element.column)
+                if !issueElements.contains(element)
+                {
+                    queue.append(element)
+                }
+            }
+            
+            if (element.row + 1) < m
+            {
+                let element = Element(row: element.row + 1, column: element.column)
+                if !issueElements.contains(element)
+                {
+                    queue.append(element)
+                }
+            }
+            
+            if (element.column - 1) >= 0
+            {
+                let element = Element(row: element.row, column: element.column - 1)
+                if !issueElements.contains(element)
+                {
+                    queue.append(element)
+                }
+            }
+            
+            if (element.column + 1) < n
+            {
+                let element = Element(row: element.row, column: element.column + 1)
+                if !issueElements.contains(element)
+                {
+                    queue.append(element)
+                }
+            }
+        }
+        return resulIimage
     }
+}
+
+struct Element: Equatable
+{
+    var row: Int
+    var column: Int
 }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
@@ -1,18 +1,31 @@
 import Foundation
 
-final class CallStation { }
+final class CallStation
+{
+    var setUsers = Set<User>()
+    var callUsers = [CallID: Call]()
+    var currentCallUsers = [UUID: Call]()
+}
 
-extension CallStation: Station {
-    func users() -> [User] {
-        []
+extension CallStation: Station
+{
+    func users() -> [User]
+    {
+        let arrayUsers = Array (setUsers)
+        return arrayUsers
     }
     
-    func add(user: User) {
-
+    func add(user: User)
+    {
+        if !setUsers.contains(user)
+        {
+            setUsers.update(with: user)
+        }
     }
     
-    func remove(user: User) {
-
+    func remove(user: User)
+    {
+        setUsers.remove(user)
     }
     
     func execute(action: CallAction) -> CallID? {
@@ -20,18 +33,21 @@ extension CallStation: Station {
     }
     
     func calls() -> [Call] {
+        let arrayCalls = Array (callUsers.values)
+        return arrayCalls
+    }
+    
+    func calls(user: User) -> [Call]{
         []
     }
     
-    func calls(user: User) -> [Call] {
-        []
+    func call(id: CallID) -> Call?
+    {
+        return callUsers[id]
     }
     
-    func call(id: CallID) -> Call? {
-        nil
-    }
-    
-    func currentCall(user: User) -> Call? {
-        nil
+    func currentCall(user: User) -> Call?
+    {
+        return currentCallUsers [user.id]
     }
 }
